c6d58d671e2b2b1981228da50c8e900a4e54657b
Benjamin-Norton <bawnorton@me.com>
2023-07-07 08:20:20 -0400

    Fix Client-side code running on server

--- common/src/main/java/com/bawnorton/allthetrims/mixin/client/ArmorTrimMixin.java
+++ common/src/main/java/com/bawnorton/allthetrims/mixin/client/ArmorTrimMixin.java
@@ -3,30 +3,39 @@ package com.bawnorton.allthetrims.mixin.client;
 import com.bawnorton.allthetrims.AllTheTrims;
 import com.bawnorton.allthetrims.client.util.ImageUtil;
 import com.bawnorton.allthetrims.client.util.PaletteHelper;
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.trim.ArmorTrim;
 import net.minecraft.item.trim.ArmorTrimMaterial;
+import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(ArmorTrimMaterial.class)
-public abstract class ArmorTrimMaterialMixin {
+import java.util.List;
+import java.util.Optional;
+
+@Mixin(ArmorTrim.class)
+public abstract class ArmorTrimMixin {
     @Shadow
-    @Final
-    private RegistryEntry<Item> ingredient;
+    public static Optional<ArmorTrim> getTrim(DynamicRegistryManager registryManager, ItemStack stack) {
+        throw new AssertionError();
+    }
 
-    @Shadow @Final private String assetName;
-
-    @ModifyReturnValue(method = "description", at = @At("RETURN"))
-    private Text updateColour(Text original) {
+    @ModifyExpressionValue(method = "appendTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/text/MutableText;append(Lnet/minecraft/text/Text;)Lnet/minecraft/text/MutableText;"))
+    private static MutableText updateColour(MutableText original, ItemStack stack, DynamicRegistryManager registryManager, List<Text> tooltip) {
+        ArmorTrim trim = getTrim(registryManager, stack).orElseThrow(AssertionError::new);
+        ArmorTrimMaterial material = trim.getMaterial().value();
+        RegistryEntry<Item> ingredient = material.ingredient();
+        String assetName = material.assetName();
         if(AllTheTrims.notWhitelisted(ingredient.value())) return Text.translatable("item.allthetrims.materials.unavailable").setStyle(original.getStyle().withColor(Formatting.RED));
         Item trimItem = ingredient.value();
         Identifier trimAssetId = new Identifier(Registries.ITEM.getId(trimItem).getNamespace(), assetName);
