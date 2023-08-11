c05d31260a0aae17abfc1a128ea19999c3be30d1
Benjamin-Norton <bawnorton@me.com>
2023-08-11 15:35:13 -0400

    use implicit local

--- common/src/main/java/com/bawnorton/allthetrims/mixin/client/ModelOverrideListMixin.java
+++ common/src/main/java/com/bawnorton/allthetrims/mixin/client/ModelOverrideListMixin.java
@@ -1,10 +1,10 @@
 package com.bawnorton.allthetrims.mixin.client;
 
 import com.bawnorton.allthetrims.Compat;
-import com.bawnorton.allthetrims.annotation.ConditionalMixin;
 import com.bawnorton.allthetrims.client.extend.InlinedConditionExtender;
 import com.bawnorton.allthetrims.client.extend.ModelOverrideConditionExtender;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelOverride;
@@ -18,7 +18,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -28,22 +27,13 @@ import static com.bawnorton.allthetrims.client.AllTheTrimsClient.MATERIAL;
 
 @Mixin(ModelOverrideList.class)
 public abstract class ModelOverrideListMixin {
-    @Unique
-    private static ModelOverrideList.BakedOverride allTheTrims$bakedOverride = null;
-
     @Inject(method = "method_33696", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/model/json/ModelOverrideList$InlinedCondition;<init>(IF)V"))
     private static void captureMaterial(Object2IntMap<?> map, ModelOverride.Condition condition, CallbackInfoReturnable<?> cir) {
         MATERIAL.set(((ModelOverrideConditionExtender) condition).allTheTrims$getMaterial());
     }
 
-    @ModifyVariable(method = "apply", at = @At("LOAD"))
-    private ModelOverrideList.BakedOverride captureBakedOverride(ModelOverrideList.BakedOverride value) {
-        allTheTrims$bakedOverride = value;
-        return value;
-    }
-
     @ModifyExpressionValue(method = "apply", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/model/json/ModelOverrideList$BakedOverride;test([F)Z"))
-    private boolean matchCustomPredicate(boolean original, BakedModel model, ItemStack stack, ClientWorld world, LivingEntity entity, int seed) {
+    private boolean matchCustomPredicate(boolean original, BakedModel model, ItemStack stack, ClientWorld world, LivingEntity entity, int seed, @Local ModelOverrideList.BakedOverride bakedOverride) {
         if(Compat.isDynamicTrimLoaded()) return original;
         if(!(stack.getItem() instanceof ArmorItem)) return original;
         if(world == null) return original;
@@ -54,7 +44,7 @@ public abstract class ModelOverrideListMixin {
         ArmorTrim trim = optionalTrim.get();
         String assetName = trim.getMaterial().value().assetName();
 
-        ModelOverrideList.InlinedCondition[] conditions = allTheTrims$bakedOverride.conditions;
+        ModelOverrideList.InlinedCondition[] conditions = bakedOverride.conditions;
         for (ModelOverrideList.InlinedCondition condition : conditions) {
             if (!(condition instanceof InlinedConditionExtender extender)) continue;
 
