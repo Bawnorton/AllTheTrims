c16613a1753f3b31e15a5e9ddda1f654a5766112
Bawnorton <bawnorton@me.com>
2024-09-10 07:52:34 -0700

    upgrade stonecutter

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,6 +1,7 @@
 pluginManagement {
     repositories {
         maven("https://maven.kikugie.dev/releases/")
+        maven("https://maven.kikugie.dev/snapshots/")
         mavenCentral()
         gradlePluginPortal()
     }
