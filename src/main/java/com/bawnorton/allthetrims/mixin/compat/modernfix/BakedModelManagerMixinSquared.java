84561f937369f2f5623637e23fcb9de272df712c
Bawnorton <bawnorton@me.com>
2024-09-09 20:56:58 -0700

    add reason

--- src/main/java/com/bawnorton/allthetrims/mixin/compat/modernfix/BakedModelManagerMixinSquared.java
+++ src/main/java/com/bawnorton/allthetrims/mixin/compat/modernfix/BakedModelManagerMixinSquared.java
@@ -17,6 +17,16 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
+/**
+ * Modernfix's dynamic resources option greatly improves performance, however, ATT relies on being able to inject
+ * models at runtime based on the models that are being loaded. This allows ATT to create the dynamic trim override
+ * for any given piece of equipment and add the predicate to the loaded model. Now since Modernfix redirects the model
+ * loading to instead rely on loading the models lazily, it skips over ATT's model injection, and thus, I need to inject
+ * into their model loader to add the dynamic trim predicate override to any equipment models (and also provide the
+ * model for the dynamic trim)
+ * Ideally Modernfix would provide an API for this, but they don't, and stated that they wouldn't so I have to resort to
+ * mixin'ing into their own mixin
+ */
 @SuppressWarnings("UnusedMixin")
 @Mixin(value = BakedModelManager.class, priority = 1500)
 @ConditionalMixin("modernfix")
